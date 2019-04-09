be8161a9977c4470d955257ead2dea13ce50c856
ctrlaltelite2012 <32407790+ctrlaltelite2012@users.noreply.github.com>
2019-04-09 12:20:45 -0600

    Update sample_code_in_shell_script.sh

--- sample_code_in_shell_script/sample_code_in_shell_script.sh
+++ sample_code_in_shell_script/sample_code_in_shell_script.sh
@@ -1,3 +1,7 @@
+#The shell script below show how to access the NCM REST API to view account data. It makes use of
+#Environment variables to set the NCM and CP credentials and uses the CURL program to access the
+#NCM "accounts" REST endpoint.
+
 ECM_API_ID="<ECM-API-ID HERE>"
 ECM_API_KEY="<ECM-API-KEY HERE>"
 CP_API_ID="<CP-API-ID HERE>"
