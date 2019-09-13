5149bb8eccf04489a369eb85d771f38b9bea1de7
mjourdenais <mjourdenais@cradlepoint.com>
2019-09-13 13:48:01 -0600

    Add API samples for monitoring use cases.

---
+++ monitoring/utils/credentials.py
@@ -0,0 +1,20 @@
+"""
+This file stores API credentials used by the samples. The samples call
+get_credentials() to retrieve them.
+
+IN REAL LIFE:
+You may not want to store your keys in plaintext in a source code file.
+You can just replace get_credentials() with code to securely retrieve your
+keys from wherever you keep them.
+"""
+
+my_creds = {
+    'cp_api_id': 'MY_CP_API_ID',  # replace me
+    'cp_api_key': 'MY_CP_API_KEY',  # replace me
+    'ecm_api_id': 'MY_ECM_API_ID',  # replace me
+    'ecm_api_key': 'MY_ECM_API_KEY',  # replace me
+}
+
+
+def get_credentials():
+    return my_creds
---
+++ monitoring/utils/logger.py
@@ -0,0 +1,25 @@
+"""
+This file contains sample code to set up a rudimentary logger that sends
+formatted text to stdout.
+
+IN REAL LIFE:
+You would probably update this to log to a file in a location and
+format that is consistent with your other logs.
+"""
+import logging
+import sys
+
+
+def get_logger():
+    """
+    :return: python logger object
+    """
+    logger = logging.getLogger('mysample')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s|%(name)s|%(levelname)s|%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
