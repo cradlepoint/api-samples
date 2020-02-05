047ebee674f4685e150aa1c0cae86e975f11d8de
Harvey Breaux <hbreauxv@gmail.com>
2020-02-05 12:28:03 -0700

    add DMVPN deploment example

---
+++ configuration/samples/dmvpn_maker/headers/ncm_headers.py
@@ -0,0 +1,17 @@
+"""
+This file stores API headers used by the samples. The samples call
+the dictionary in this script to retrieve them.  You can store multiple
+header dictionaries here and call the one you want to use in your script.
+
+IN PRODUCTION:
+You may not want to store your keys in plaintext in a source code file.
+You can just replace myheaders with code to securely retrieve your
+keys from wherever you keep them.
+"""
+
+myheader = {
+    'cp_api_id': 'MY_CP_API_ID',  # replace me
+    'cp_api_key': 'MY_CP_API_KEY',  # replace me
+    'ecm_api_id': 'MY_ECM_API_ID',  # replace me
+    'ecm_api_key': 'MY_ECM_API_KEY',  # replace me
+}
---
+++ configuration/samples/dmvpn_maker/restore_configurations.py
@@ -0,0 +1,83 @@
+# Patches an "Undo" configuration that removes all settings that were patched by config_pusher.py
+
+import json
+import csv
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from headers import ncm_headers
+
+# Define headers
+headers = ncm_headers.myheader
+
+# Load ctrl-z payload
+with open("./configs/config_backups/undo_changes.json", "r") as j:
+    undo_payload = json.loads(j.read())
+
+def restore_configs():
+    """Pushes a config to remove all the additions from config_pusher.py"""
+
+    # open CSV to read router ID's
+    with open('test_routers.csv', 'r') as f:
+        config_csv = csv.reader(f)
+
+        # open session to ncm
+        with requests.Session() as s:
+            # Set max retries to 3
+            retries = Retry(total=3,
+                            backoff_factor=0.2,
+                            status_forcelist=[500, 502, 503, 504])
+
+            s.mount('https://', HTTPAdapter(max_retries=retries))
+
+            # Iterate through csv and patch config to routers
+            for row, column in enumerate(config_csv):
+
+                # skip header row
+                if row == 0:
+                    continue
+
+                # exit when out of router ids
+                if column[0] == '':
+                    print("End of router id's reached, exiting program")
+                    break  # No router id, exit program
+
+                # get router id
+                router_id = int(column[0])
+
+                # initialize configuration_managers id
+                cfg_id = ''
+
+                try:
+                    # Use this payload as the payload for this router
+                    payload = undo_payload
+
+                    # get configuration_managers id
+                    cfg_id = \
+                    s.get('https://cradlepointecm.com/api/v2/configuration_managers/?router.id={}'.format(router_id),
+                          headers=headers).json()
+                    cfg_id = cfg_id.get("data")[0].get("id")
+
+                    # patch payload
+                    patch = s.patch('https://cradlepointecm.com/api/v2/configuration_managers/{}/'.format(cfg_id),
+                                    headers=headers, data=json.dumps(payload))
+
+                    # print result
+                    patch_result = patch.status_code
+                    print('Config patch sent to router {}\nResponse = {}\n'.format(router_id, patch_result))
+
+                    # Catch failed status codes and log them
+                    if patch_result is not 202:
+                        print("Patch unsuccessful. Status code {}, Response: {}\n".format(patch_result, patch.text))
+
+                # catch index errors, probably from a failed GET
+                except IndexError as e:
+                    print('{} IndexError, probably from failed GET. Exception = {}\nGET Response = {}\n'.format(router_id, e, cfg_id))
+
+                # catch very broad exceptions
+                except Exception as e:
+                    print('Error patching to {}\nException = {}\n'.format(router_id, e))
+
+
+if __name__ == '__main__':
+    restore_configs()
