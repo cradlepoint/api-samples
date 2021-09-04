14d684bceaa9af7058ac95ec4236825142f701c5
Alex Terrell <phate999@gmail.com>
2021-09-04 06:50:13 -0600

    Added comments

--- historical_locations_export/historical_locations_export.py
+++ historical_locations_export/historical_locations_export.py
@@ -1,3 +1,7 @@
+# historical_locations_export.py
+# Enter Cradlepoint NCM API Keys below to export data from the historical_locations/ endpoint to .csv file
+# Drag and drop .csv file to https://kepler.gl/demo to create maps based on data such as signal strength
+
 import requests
 import csv
 
@@ -12,22 +16,20 @@ headers = {'X-ECM-API-ID': 'YOUR',
            'X-CP-API-KEY': 'HERE',
            'Content-Type': 'application/json'}
 
-top_line = ["router", "name", "accuracy", "carrier_id", "cinr", "created_at", "created_at_timeuuid", "dbm", "ecio",
-            "latitude",
-            "longitude", "mph", "net_device_name", "rfband", "rfband_5g", "rsrp", "rsrp_5g", "rsrq", "rsrq_5g",
-            "rssi", "signal_percent", "sinr", "sinr_5g", "summary"]
-
+col_headings = ["router", "name", "accuracy", "carrier_id", "cinr", "created_at", "created_at_timeuuid", "dbm", "ecio",
+                "latitude",
+                "longitude", "mph", "net_device_name", "rfband", "rfband_5g", "rsrp", "rsrp_5g", "rsrq", "rsrq_5g",
+                "rssi", "signal_percent", "sinr", "sinr_5g", "summary"]
 
 with open(output_file, 'wt') as f:
     writer = csv.writer(f, delimiter=',')
-    writer.writerow(top_line)  # write header
+    writer.writerow(col_headings)  # write header
 
     routers_url = f'{server}/api/v2/routers/?state__in=online,offline&limit=500'
     try:
         while routers_url:
             routers_req = requests.get(routers_url, headers=headers).json()
             routers = routers_req['data']
-            print(routers)
             for router in routers:
                 locations_url = f'{server}/api/v2/historical_locations/?router=' \
                     f'{router["id"]}&created_at__gt={start_date}'
