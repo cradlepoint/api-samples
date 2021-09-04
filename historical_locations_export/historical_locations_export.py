35f7f248317c403b5eef49fb62c4bf1098f2be68
Alex Terrell <phate999@gmail.com>
2021-09-04 07:56:23 -0600

    Added comments

--- historical_locations_export/historical_locations_export.py
+++ historical_locations_export/historical_locations_export.py
@@ -1,21 +1,23 @@
 # historical_locations_export.py
 # Enter Cradlepoint NCM API Keys below to export data from the historical_locations/ endpoint to .csv file
+# Set start_date and end_date for dates you want to export data for
 # Drag and drop .csv file to https://kepler.gl/demo to create maps based on data such as signal strength
 
 import requests
 import csv
 
-start_date = '2021-09-02'
-
-server = 'https://www.cradlepointecm.com'
-output_file = 'historical_locations.csv'
-
-headers = {'X-ECM-API-ID': 'YOUR',
+api_keys = {'X-ECM-API-ID': 'YOUR',
            'X-ECM-API-KEY': 'KEYS',
            'X-CP-API-ID': 'GO',
            'X-CP-API-KEY': 'HERE',
            'Content-Type': 'application/json'}
 
+start_date = '2021-09-01'
+end_date = '2021-09-04'
+
+output_file = 'historical_locations.csv'
+
+server = 'https://www.cradlepointecm.com'
 col_headings = ["router", "name", "accuracy", "carrier_id", "cinr", "created_at", "created_at_timeuuid", "dbm", "ecio",
                 "latitude",
                 "longitude", "mph", "net_device_name", "rfband", "rfband_5g", "rsrp", "rsrp_5g", "rsrq", "rsrq_5g",
@@ -28,13 +30,13 @@ with open(output_file, 'wt') as f:
     routers_url = f'{server}/api/v2/routers/?state__in=online,offline&limit=500'
     try:
         while routers_url:
-            routers_req = requests.get(routers_url, headers=headers).json()
+            routers_req = requests.get(routers_url, headers=api_keys).json()
             routers = routers_req['data']
             for router in routers:
                 locations_url = f'{server}/api/v2/historical_locations/?router=' \
-                    f'{router["id"]}&created_at__gt={start_date}'
+                    f'{router["id"]}&created_at__gt={start_date}&created_at__lt={end_date}'
                 while locations_url:
-                    locations_req = requests.get(locations_url, headers=headers).json()
+                    locations_req = requests.get(locations_url, headers=api_keys).json()
                     locations = locations_req['data']
                     for location in locations:
                         if location['carrier_id']:
