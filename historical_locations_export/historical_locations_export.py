4508fc4cd5544da3350b88c719c3b819a6c70362
Alex Terrell <phate999@gmail.com>
2021-09-15 11:25:24 -0600

    Modified to conform to PEP8 and PEP257 standards

--- historical_locations_export/historical_locations_export.py
+++ historical_locations_export/historical_locations_export.py
@@ -1,42 +1,58 @@
-# historical_locations_export.py
-# Enter Cradlepoint NCM API Keys below to export data from the historical_locations/ endpoint to .csv file
-# Set start_date and end_date for dates you want to export data for
-# Drag and drop .csv file to https://kepler.gl/demo to create maps based on data such as signal strength
+"""Export NCM API historical_locations/ data to .csv file
+
+This script will export the data from the Cradlepoint NCM API
+historical_locations/ endpoint for all devices for the given date range.
+Data is written to the .csv file defined below.
+Cradlepoint NCM API keys are required.
+
+Attributes
+----------
+start_date : beginning of date range to export
+end_date : end of date range to export
+output_file : name of .csv file to be created
+headers : Replace values with your NCM API keys for use in HTTP requests.
+"""
 
 import requests
 import csv
 
-api_keys = {'X-ECM-API-ID': 'YOUR',
+start_date = '2021-09-02'
+end_date = '2021-09-03'
+
+server = 'https://www.cradlepointecm.com'
+output_file = 'historical_locations.csv'
+
+headers = {'X-ECM-API-ID': 'YOUR',
            'X-ECM-API-KEY': 'KEYS',
            'X-CP-API-ID': 'GO',
            'X-CP-API-KEY': 'HERE',
            'Content-Type': 'application/json'}
 
-start_date = '2021-09-01'
-end_date = '2021-09-04'
-
-output_file = 'historical_locations.csv'
-
-server = 'https://www.cradlepointecm.com'
-col_headings = ["router", "name", "accuracy", "carrier_id", "cinr", "created_at", "created_at_timeuuid", "dbm", "ecio",
-                "latitude",
-                "longitude", "mph", "net_device_name", "rfband", "rfband_5g", "rsrp", "rsrp_5g", "rsrq", "rsrq_5g",
-                "rssi", "signal_percent", "sinr", "sinr_5g", "summary"]
+top_line = ["router", "name", "accuracy", "carrier_id", "cinr",
+            "created_at", "created_at_timeuuid", "dbm", "ecio",
+            "latitude", "longitude", "mph", "net_device_name",
+            "rfband", "rfband_5g", "rsrp", "rsrp_5g", "rsrq",
+            "rsrq_5g", "rssi", "signal_percent", "sinr",
+            "sinr_5g", "summary"]
 
 with open(output_file, 'wt') as f:
     writer = csv.writer(f, delimiter=',')
-    writer.writerow(col_headings)  # write header
+    writer.writerow(top_line)  # write header
 
-    routers_url = f'{server}/api/v2/routers/?state__in=online,offline&limit=500'
+    routers_url = f'{server}/api/v2/routers/?state__in=online,offline' \
+        f'&limit=500'
     try:
         while routers_url:
-            routers_req = requests.get(routers_url, headers=api_keys).json()
+            routers_req = requests.get(routers_url, headers=headers).json()
             routers = routers_req['data']
+            print(routers)
             for router in routers:
-                locations_url = f'{server}/api/v2/historical_locations/?router=' \
-                    f'{router["id"]}&created_at__gt={start_date}&created_at__lt={end_date}'
+                locations_url = f'{server}/api/v2/historical_locations/' \
+                    f'?router={router["id"]}&created_at__gt={start_date}' \
+                    f'&created_at__lt={end_date}'
                 while locations_url:
-                    locations_req = requests.get(locations_url, headers=api_keys).json()
+                    locations_req = requests.get(
+                        locations_url, headers=headers).json()
                     locations = locations_req['data']
                     for location in locations:
                         if location['carrier_id']:
