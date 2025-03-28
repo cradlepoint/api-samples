b9830ac1730f0f07adf5acd9f268c2d2cf9c1e2e
Github Action <github_action@example.com>
2025-03-28 18:50:50 +0000

    Bump version: 0.0.38 → 0.0.39

--- ncm/setup.py
+++ ncm/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ncm",
-    version="0.0.38",
+    version="0.0.39",
     author="Nathan Wiens - Cradlepoint",
     author_email="nathan.wiens@cradlepoint.com",
     description="Python client library for Cradlepoint NCM API",
